19a3606827f8b7b2af09b26092d5c4798c182ba6
Alex Slape <aslape@atlassian.com>
2019-07-22 09:24:09 -0500

    Updating stationcheck to satisfy linter

--- pkg/stationcheck/stationcheck.py
+++ pkg/stationcheck/stationcheck.py
@@ -1,15 +1,19 @@
+'''A workstation check for up-to-date packages and languages'''
 #!/usr/bin/env python3
 
-from packaging import version #setuptools
-import yaml
 import subprocess
 import argparse
 import os
+from packaging import version #setuptools
+import yaml
+
 
 PACKAGE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 REQ_FILE = os.path.join(PACKAGE_DIR, "pkg/config/requirements.yaml")
 
+#pylint: disable=too-few-public-methods, invalid-name
 class bcolors:
+    '''A strucut for commonly used terminal colors'''
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -20,9 +24,11 @@ class bcolors:
     UNDERLINE = '\033[4m'
     VIOLET = '\033[35m'
 
+#pylint: disable=invalid-name
 class station_check:
     '''Checks, updates, and otherwise sets up a SecDev workstation to meet our requirements'''
 
+    #pylint: disable=unused-argument
     def __init__(self, config_file=REQ_FILE, verbose=False, test=False):
         '''Reads in requirements YAML file, checks versions, and installs
         out-of-date or missing packages.'''
@@ -41,11 +47,11 @@ class station_check:
         for package in config["packages"]:
             if package != "default":
                 print("%s%s %s%s%s check: %s" % (bcolors.OKBLUE,
-                                             config["packages"][package]["display"],
-                                             bcolors.VIOLET,
-                                             config["requirements"][package],
-                                             bcolors.OKBLUE,
-                                             bcolors.ENDC), end='')
+                                                 config["packages"][package]["display"],
+                                                 bcolors.VIOLET,
+                                                 config["requirements"][package],
+                                                 bcolors.OKBLUE,
+                                                 bcolors.ENDC), end='')
                 v_check = self.version_check(config["packages"][package]["command"],
                                              config["packages"][package]["version_extraction"],
                                              config["requirements"][package])
@@ -54,9 +60,10 @@ class station_check:
                     successes += 1
                 elif not v_check and not v_check == "invalid":
 
-                    print("%sInstalling newer version of %s...%s" % (bcolors.WARNING,
-                                                                     config["packages"][package]["display"],
-                                                                     bcolors.ENDC))
+                    print("%sInstalling newer version of %s...%s" %
+                          (bcolors.WARNING,
+                           config["packages"][package]["display"],
+                           bcolors.ENDC))
 
                     if "installer" not in config["packages"][package]:
                         installer = config["packages"]["default"]["installer"]
@@ -69,7 +76,7 @@ class station_check:
                         command = config["packages"][package]["command"]
 
                     stdout, err = self.install_package(command,
-                                        config["installers"][installer]["command"])
+                                                       config["installers"][installer]["command"])
 
                     if err == 0:
                         successes += 1
@@ -78,23 +85,25 @@ class station_check:
                     else:
                         failures += 1
                         print("%sInstallation failed.%s" % (bcolors.FAIL, bcolors.ENDC))
+                        print(stdout)
 
                 print("--------------------")
 
         self.print_results(successes, failures, installs)
 
-
+    #pylint: disable=no-self-use
     def print_results(self, successes, failures, installs):
         '''Prints outcome of workstation check'''
         total = successes + failures
         print("%sPackages installed: %s%s%s" % (bcolors.OKGREEN,
-                                          bcolors.VIOLET,
-                                          str(installs),
-                                          bcolors.ENDC))
+                                                bcolors.VIOLET,
+                                                str(installs),
+                                                bcolors.ENDC))
+        #pylint: disable=literal-comparison
         if failures is not 0:
             print("%sInstallations failed: %s%s" % (bcolors.WARNING,
-                                                   str(failures),
-                                                   bcolors.ENDC))
+                                                    str(failures),
+                                                    bcolors.ENDC))
             print("%sFAIL: %s packages out of %s meet requirements.%s" % (bcolors.FAIL,
                                                                           str(successes),
                                                                           str(total),
@@ -108,21 +117,22 @@ class station_check:
     def bash(self, command):
         '''Evaluates a string as a bash command.'''
         run = subprocess.run(command,
-            shell=True,
-            text=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            executable='/bin/bash')
+                             shell=True,
+                             text=True,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             executable='/bin/bash')
 
+        #pylint: disable=literal-comparison
         if run.returncode is not 0:
 
-            print ("%sExit code: %s%s" % (bcolors.FAIL,
-                                          str(run.returncode),
-                                          bcolors.ENDC))
+            print("%sExit code: %s%s" % (bcolors.FAIL,
+                                         str(run.returncode),
+                                         bcolors.ENDC))
 
-            print ("%sFailed to run: %s%s" % (bcolors.FAIL,
-                                              command,
-                                              bcolors.ENDC))
+            print("%sFailed to run: %s%s" % (bcolors.FAIL,
+                                             command,
+                                             bcolors.ENDC))
 
         return run.stdout, run.returncode
 
@@ -149,7 +159,7 @@ class station_check:
 
             if str(extracted) == "":
                 return False
-            print ("%sExtracted version number (%s) appears to be an invalid format. \
+            print("%sExtracted version number (%s) appears to be an invalid format. \
                     Consider updating your version extraction command.%s" % (bcolors.FAIL,
                                                                              str(extracted),
                                                                              bcolors.ENDC))
@@ -158,13 +168,13 @@ class station_check:
 
 
         if more_recent:
-            print ("%sFound %s!%s" % (bcolors.OKGREEN,
-                                              extracted,
-                                              bcolors.ENDC))
+            print("%sFound %s!%s" % (bcolors.OKGREEN,
+                                     extracted,
+                                     bcolors.ENDC))
         else:
-            print ("%sFound %s!%s" % (bcolors.WARNING,
-                                              extracted,
-                                              bcolors.ENDC))
+            print("%sFound %s!%s" % (bcolors.WARNING,
+                                     extracted,
+                                     bcolors.ENDC))
 
         return more_recent
 
@@ -174,16 +184,18 @@ class station_check:
         return self.bash(install_script + " " + install_name)
 
     def install_configs(self, config_list):
+        '''Runs configuration commands listed under an installer in the config file'''
         for configuration in config_list:
             stdout, err = self.bash(configuration)
+            #pylint: disable=literal-comparison
             if err is not 0:
-                print ("%sFailed to run: %s%s" % (bcolors.FAIL,
-                                                  bcolors.ENDC,
-                                                  configuration))
-                return False
-            print ("%sInstaller configuration: %s%s" % (bcolors.OKGREEN,
+                print("%sFailed to run: %s%s" % (bcolors.FAIL,
                                                  bcolors.ENDC,
                                                  configuration))
+                return False
+            print("%sInstaller configuration: %s%s" % (bcolors.OKGREEN,
+                                                       bcolors.ENDC,
+                                                       configuration))
         return True
 
 if __name__ == "__main__":
@@ -202,5 +214,3 @@ if __name__ == "__main__":
             ARGDICT[arg] = ARGS[arg]
 
     station_check(**ARGDICT)
-
-
